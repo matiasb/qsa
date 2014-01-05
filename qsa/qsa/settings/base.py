a39cb6eb90fa72de7fac7d2772a8396112abd45b
nessita <nataliabidart@gmail.com>
2014-01-05 13:59:39 -0200

    Removed github from social integration.

--- qsa/qsa/settings/base.py
+++ qsa/qsa/settings/base.py
@@ -151,7 +151,7 @@ INSTALLED_APPS = (
     'allauth.socialaccount',
     # ... include the providers you want to enable:
     ###'allauth.socialaccount.providers.facebook',
-    'allauth.socialaccount.providers.github',
+    ###'allauth.socialaccount.providers.github',
     ###'allauth.socialaccount.providers.google',
     ###'allauth.socialaccount.providers.linkedin',
     ###'allauth.socialaccount.providers.openid',
