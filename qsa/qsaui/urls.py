aa6bdc98ad3c14964023fe98597ae6f842f031b0
nessita <nataliabidart@gmail.com>
2014-01-12 21:38:06 -0200

    - Added series detail template and view.

--- qsa/qsaui/urls.py
+++ qsa/qsaui/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns(
     'qsaui.views',
     url(r'^$', 'home', name='home'),
     url(r'^search/$', 'search', name='search'),
+    url(r'^(?P<tvdb_id>\d+)/$', 'series_detail', name='series-detail'),
     url(r'^add/$', 'add_to_watchlist', name='add-to-watchlist'),
 )
