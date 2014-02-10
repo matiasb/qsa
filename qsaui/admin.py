f28bb2770e1f3877ce7ee588f188a1541b717a44
nessita <nataliabidart@gmail.com>
2014-02-10 12:06:53 -0200

    Improved admins sites for Episode and Series to easu usage.

--- qsaui/admin.py
+++ qsaui/admin.py
@@ -18,5 +18,20 @@ class UserAdmin(UserAdmin):
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 
-admin.site.register(Series)
-admin.site.register(Episode)
+
+class SeriesAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'runtime', 'network', 'tvdb_id', 'imdb_id', 'completed')
+    list_filter = ('runtime', 'network', 'completed')
+    search_fields = ('name',)
+
+
+class EpisodeAdmin(admin.ModelAdmin):
+    date_hierarchy = 'first_aired'
+    list_display = ('series', 'season', 'number', 'tvdb_id', 'imdb_id')
+    list_filter = ('series', 'season')
+    search_fields = ('series__name', 'season')
+
+
+admin.site.register(Episode, EpisodeAdmin)
+admin.site.register(Series, SeriesAdmin)
