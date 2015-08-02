00265cfe5e92f76c9700b8d2f27987f595fd9973
nessita <nataliabidart@gmail.com>
2015-08-02 18:23:57 -0300

    No more custom context processors for allauth.

--- qsa/settings/base.py
+++ qsa/settings/base.py
@@ -90,19 +90,6 @@ USE_TZ = False
 STATIC_ROOT = os.path.join(BASE_DIR, 'qsa', 'static')
 STATIC_URL = '/static/'
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.debug',
-    'django.core.context_processors.i18n',
-    'django.core.context_processors.media',
-    'django.core.context_processors.request',
-    'django.core.context_processors.static',
-    'django.core.context_processors.tz',
-    'django.contrib.messages.context_processors.messages',
-    'allauth.account.context_processors.account',
-    'allauth.socialaccount.context_processors.socialaccount',
-)
-
 SITE_ID = 1
 
 # Parse database configuration from $DATABASE_URL
